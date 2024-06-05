801e161a6f7d519cc0cf08c9f5feba9e426c23b3
KashTheKing <kashtheking123@gmail.com>
2024-06-04 21:41:17 -0700

    Added json functions: - json.stringify(jsonContent) - json.parse(string)

---
+++ webxTypes.lua
@@ -0,0 +1,26 @@
+type HtmlElement = {
+    on_click: (callback: () -> ()) -> (),
+    on_input: (callback: () -> ()) -> (),
+    on_submit: (callback: () -> ()) -> (),
+    get_content: () -> string,
+    set_content: (content: string) -> (),
+    get_href: () -> string,
+    set_href: (href: string) -> (),
+    get_source: () -> string,
+    set_source: (source: string) -> (),
+    get_opacity: () -> number,
+    set_opacity: (opacity: number) -> (),
+    set_visible: (visible: boolean) -> (),
+}
+
+type FetchOptions = {
+    url: string,
+    method: string,
+    headers: {},
+    body: string,
+}
+
+declare function print(message: string)
+declare function get(class: string): HtmlElement
+declare function fetch(options: FetchOptions): string
+declare function set_timeout(ms: number, callback: () -> ())
